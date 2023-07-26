e7fac8ca5d266908a793bc019619e4000f4bdec5
JeffreyDinackus <jeff.dinackus@proton.me>
2023-07-26 18:03:02 -0400

    disclaimer added

--- Mac/QOTD.sh
+++ Mac/QOTD.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#this has not been tested as the developer doesn't have a Mac
+
 
 function get_random_quote() {
     local response=$(curl -s "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
