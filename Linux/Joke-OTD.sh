07bca63ad26e9ec35a621ea7e84504346d07032a
JeffreyDinackus <jeffreydinackus@proton.me>
2023-07-26 22:58:43 -0400

    joke of the day for linux added

---
+++ Linux/Joke-OTD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while true; do
+  joke=$(curl -s "https://v2.jokeapi.dev/joke/Any")
+
+  joke_setup=$(echo "$joke" | jq -r '.setup')
+  joke_delivery=$(echo "$joke" | jq -r '.delivery')
+
+  # Check if both setup and delivery are not null
+  if [ "$joke_setup" != "null" ] && [ "$joke_delivery" != "null" ]; then
+    echo "$joke_setup"
+    echo "$joke_delivery"
+    break # Exit the loop if a valid joke is fetched
+  else
+    echo "Failed to fetch a complete joke. Trying again..."
+    sleep 1 # Wait for 1 second before retrying
+  fi
+done
--- Linux/QOTD.sh
+++ Linux/QOTD.sh
@@ -3,6 +3,8 @@
 # Fetch the quote from the Forismatic API using curl
 quote=$(curl -s "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
 
+print(quote)
+
 # Extract the quote text and author from the JSON response
 quote_text=$(echo "$quote" | grep -o '"quoteText":"[^"]*' | cut -d'"' -f4)
 quote_author=$(echo "$quote" | grep -o '"quoteAuthor":"[^"]*' | cut -d'"' -f4)
