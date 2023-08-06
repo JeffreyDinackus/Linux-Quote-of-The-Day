e46664ff0a3c204ad4975869cb8a091580ecdfd6
JeffreyDinackus <jeffreydinackus@proton.me>
2023-08-06 15:35:56 -0400

    fixing typos, 404 page added

--- Linux/QOTD.sh
+++ Linux/QOTD.sh
@@ -3,14 +3,11 @@
 # Fetch the quote from the Forismatic API using curl
 quote=$(curl -s "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
 
-print(quote)
-
 # Extract the quote text and author from the JSON response
 quote_text=$(echo "$quote" | grep -o '"quoteText":"[^"]*' | cut -d'"' -f4)
 quote_author=$(echo "$quote" | grep -o '"quoteAuthor":"[^"]*' | cut -d'"' -f4)
 
 # Display the quote and author
-# uncomment if desired
 # echo "Quote of the day:"
 echo "$quote_text"
 echo "$quote_author"
